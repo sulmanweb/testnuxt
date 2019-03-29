8d8183a46c4848cc793b9d9d3befb3505ef2b81c
Sulman Baig <sulmanweb@gmail.com>
2019-03-29 10:30:30 +0500

    Setting the server for https

--- server/index.js
+++ server/index.js
@@ -1,34 +1,55 @@
-const express = require('express')
-const consola = require('consola')
+const http = require('http')
+const https = require('https')
 const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const app = require('express')()
+const consola = require('consola')
+const fs = require('fs-extra')
 
-// Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'production')
+let server
 
-async function start() {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+const isProd = process.env.NODE_ENV === 'production'
+const port = process.env.PORT || 3000
 
-  const { host, port } = nuxt.options.server
-
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
+// Prepare for HTTP or HTTPS
+if (process.env.NODE_ENV === 'production') {
+  const pkey = fs.readFileSync('/etc/letsencrypt/live/test.sulmanbaig.com/privkey.pem')
+  const pcert = fs.readFileSync('/etc/letsencrypt/live/test.sulmanbaig.com/cert.pem')
+  const httpsOptions = {
+    key: pkey,
+    cert: pcert
   }
+  server = https.createServer(httpsOptions, app)
+} else {
+  server = http.createServer(app)
+}
 
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
+// We instantiate nuxt.js with the options
+const config = require('../nuxt.config.js')
 
+config.dev = !isProd
+const nuxt = new Nuxt(config)
+
+// Render every route with Nuxt.js
+app.use(nuxt.render)
+
+function listen() {
   // Listen the server
-  app.listen(port, host)
+  // app.listen(port, '0.0.0.0');
+  server.listen(port, 'localhost')
   consola.ready({
-    message: `Server listening on http://${host}:${port}`,
+    message: `Server listening on http://localhost:${port}`,
     badge: true
   })
 }
-start()
+
+// Build only in dev mode with hot-reloading
+if (config.dev) {
+  new Builder(nuxt)
+    .build()
+    .then(listen)
+    .catch(() => {
+      process.exit(1)
+    })
+} else {
+  listen()
+}
